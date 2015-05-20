21c9475825813a98f0a697fd95987ff2e020f409
Andrew Muro <andrew.t.muro@gmail.com>
2015-05-20 14:05:45 -0400

    always display flash message if user isn't confirmed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,18 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  after_action :check_sms_confirmed
+
   # TODO: remove me!
   if Rails.env.production?
     http_basic_authenticate_with name: ENV['AUTH_USER'], password: ENV['AUTH_PASS']
+  end
+
+  private
+
+  def check_sms_confirmed
+    if user_signed_in? and current_user.poster? and !current_user.sms_confirmed
+      flash[:alert] = I18n.t('msg.please_confirm')
+    end
   end
 end
